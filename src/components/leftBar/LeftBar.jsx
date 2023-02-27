55e11721e84bfa7e5a6cbcb6d64c1de10d273d55
sefa can  genc <sefacan19.sg@gmail.com>
2023-02-27 23:46:11 +0300

    changes navbar

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const LeftBar = () => {
   return (
-    <div>LeftBar</div>
+    <div className='LeftBar'>LeftBar
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/7567228/pexels-photo-7567228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <span>Sefa Genc</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-import React from 'react'q
-import "./navbar.scss"
 import "./navbar.scss";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -9,33 +7,47 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { AuthContext } from "../../context/authContext";
 
 const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="left">
-        <Link to="/" style={{textDecoration:"none"}}>
-          <span>Sefasocial</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>lamasocial</span>
         </Link>
-        <HomeOutlinedIcon/>
-        <DarkModeOutlinedIcon/>
-        <GridViewOutlinedIcon/>
+        <HomeOutlinedIcon />
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} />
+        )}
+        <GridViewOutlinedIcon />
         <div className="search">
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
         </div>
       </div>
-    <div className="right">
-      <PersonOutlinedIcon/>
-      <EmailOutlinedIcon/>
-      <NotificationsOutlinedIcon/>
-      <div className="user">
-        {/* <img src="https://images.pexels.com/photos/7567228/pexels-photo-7567228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
-        <span>Sefa Genc</span>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src={currentUser.profilePic}
+            alt=""
+          />
+          <span>{currentUser.name}</span>
+        </div>
       </div>
-    </div> 
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
